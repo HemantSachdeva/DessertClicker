12548b52ade9dbdf394438b6d22b2f1c5b0442c2
Hemant Sachdeva <hemant.evolver@gmail.com>
2021-12-16 19:56:34 +0530

    Add debug logging message for `onSaveInstanceState`
     * The onSaveInstanceState() callback occurs just after onPause() and onStop()
       when we press home to close our app.
    
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -100,6 +101,11 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop Called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState called")
     }
 
     override fun onDestroy() {
